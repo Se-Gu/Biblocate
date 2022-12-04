fb96f98a19139e6e4f0a9f44963cc2b833ca3e9a
tarakciemre <tarakci.software@gmail.com>
2022-12-04 18:35:18 +0300

    added sample data

---
+++ books.js
@@ -0,0 +1,24 @@
+let books = [
+    {
+        title: "Republic",
+        author: "Plato",
+        available: 0
+    },
+    {
+        title: "Crime and Punishment",
+        author: "Dostoyevsky, Fyodor",
+        available: 4
+    },
+    {
+        title: "Genealogy of Morals",
+        author: "Nietzsche",
+        available: 0
+    },
+    {
+        title: "The Brothers Karamazov",
+        author: "Dostoyevsky, Fyodor",
+        available: 4
+    },
+];
+
+export default books;
