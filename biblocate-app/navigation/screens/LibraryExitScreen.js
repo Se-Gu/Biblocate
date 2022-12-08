7bfc21971ec96b13b376904f6f82024b11853ea8
Serhat Gürgenyatağı <63667747+Se-Gu@users.noreply.github.com>
2022-12-08 22:54:02 +0300

    Navigation from Home Screen Created

---
+++ biblocate-app/navigation/screens/LibraryExitScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const LibraryExitScreen = () => {
+  return (
+    <View>
+      <Text>LibraryExitScreen</Text>
+    </View>
+  );
+};
+
+export default LibraryExitScreen;
---
+++ biblocate-app/navigation/screens/StationaryStoreScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const StationaryStoreScreen = () => {
+  return (
+    <View>
+      <Text>StationaryStoreScreen</Text>
+    </View>
+  );
+};
+
+export default StationaryStoreScreen;
