2b8f0832b060e714f28686ca38b96716b5cdee73
Serhat Gürgenyatağı <63667747+Se-Gu@users.noreply.github.com>
2022-12-08 18:46:18 +0300

    bilkent image added to homescreen

--- biblocate-app/navigation/screens/HomeScreen.js
+++ biblocate-app/navigation/screens/HomeScreen.js
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
 import React from "react";
 
 const HomeScreen = () => {
   return (
     <View>
+      <Image
+        source={require("biblocate-app/assets/img/bilkent-library.png")}
+        style={{ opacity: 0.7 }}
+      />
       <Text>HomeScreen</Text>
     </View>
   );
