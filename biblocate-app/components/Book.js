d8ae74e3c6a8a3932b6f5b55dd7ba5b354a1bc4d
tarakciemre <tarakci.software@gmail.com>
2022-12-09 18:13:20 +0300

    Implemented details view for book

--- biblocate-app/components/Book.js
+++ biblocate-app/components/Book.js
@@ -2,6 +2,7 @@ import {
     StyleSheet,
     TouchableOpacity,
     Keyboard,
+    View,
 } from "react-native";
 import React from "react";
 import { Button, Card, Title, Paragraph } from "react-native-paper";
@@ -10,10 +11,10 @@ import { Button, Card, Title, Paragraph } from "react-native-paper";
 const Book = ({book, index, currentIndex, setCurrentIndex, setDetails, details}) => {
     
     return (
-    <Card style={ index === currentIndex ? styles.expandedBook : styles.collapsedBook}>
+        <View>
+    <Card style={ index === currentIndex ? ( details ? styles.details : styles.expandedBook) : styles.collapsedBook}>
         <TouchableOpacity 
             onPress={() => {
-                console.log('perssed')
                 setCurrentIndex(index === currentIndex ? null : index);
                 Keyboard.dismiss();
                 if(details) {setDetails(false)};
@@ -28,17 +29,28 @@ const Book = ({book, index, currentIndex, setCurrentIndex, setDetails, details})
                         <Paragraph style={{ marginBottom: 15 }}>{book.summary}</Paragraph>
                     </React.Fragment>
                 )}
+                { details && (
+                    <React.Fragment>
+                        <Paragraph>Available: {book.available}</Paragraph>
+                        <Paragraph style={{ marginBottom: 15 }}>{book.summary}</Paragraph>
+                        <Paragraph>Available: {book.available}</Paragraph>
+                        <Paragraph style={{ marginBottom: 15 }}>{book.summary}</Paragraph>
+                        <Paragraph>Available: {book.available}</Paragraph>
+                        <Paragraph style={{ marginBottom: 15 }}>{book.summary}</Paragraph>
+                        <Paragraph>Available: {book.available}</Paragraph>
+                        <Paragraph style={{ marginBottom: 15 }}>{book.summary}</Paragraph>
+                    </React.Fragment>
+                )}
             </Card.Content>
         </TouchableOpacity>
             {(index === currentIndex) && (
                 <Card.Actions>                     
                     { book.available > 0 && (
-                     <Button>Navigate</Button>
+                     <Button buttonColor="#097969" textColor="#ffffff">Navigate</Button>
                      )}
                      {!details && (
                 <Button
                     onPress={() => {
-                    console.log("asdf");
                     setDetails(true);
                     }}
                 >
@@ -48,6 +60,9 @@ const Book = ({book, index, currentIndex, setCurrentIndex, setDetails, details})
                 </Card.Actions>
             )}
     </Card>
+    <View>
+    </View>
+</View>
     )
 }
 
@@ -92,6 +107,23 @@ const styles = StyleSheet.create({
         backgroundColor: "#FDFDFC",
         padding: 0,
         marginTop: 7,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    details: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.4,
+        shadowRadius: 4,
+        elevation: 4,
+        flexGrow: 1,
+        backgroundColor: "#fff",
+        padding: 0,
+        marginTop: 7,
+        marginBottom: 30,
         marginLeft: 10,
         marginRight: 10,
     },
