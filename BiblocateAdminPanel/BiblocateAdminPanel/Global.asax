29f26d6da49e7f948d0a8bfe8cbe22ca0249ca9c
Giray-Senel <giray.senel@ug.bilkent.edu.tr>
2022-12-20 01:29:40 +0300

    Admin Panel

---
+++ BiblocateAdminPanel/BiblocateAdminPanel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BiblocateAdminPanel.MvcApplication" Language="C#" %>
---
+++ BiblocateAdminPanel/BiblocateAdminPanel/Models/RoomModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BiblocateAdminPanel.Models
+{
+    public class RoomModel
+    {
+        public string Id { get; set; }
+        public List<ShelfModel> Shelves { get; set; }
+    }
+}
