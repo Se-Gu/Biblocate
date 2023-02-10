248e0a8677a67ba8695a71d04db68ff52378ea04
Serhat Gürgenyatağı <63667747+Se-Gu@users.noreply.github.com>
2023-02-10 15:45:27 +0300

    improvements

--- biblocate-app/navigation/screens/NewBookSearchScreen.js
+++ biblocate-app/navigation/screens/NewBookSearchScreen.js
@@ -1,42 +1,52 @@
-import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  View,
-} from "react-native";
+import { StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import { SearchBar, ListItem } from "@rneui/themed";
 import { useState } from "react";
 import axios from "axios";
-import { FlashList } from "@shopify/flash-list";
+import * as Clipboard from "expo-clipboard";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const NewBookSearchScreen = () => {
   const [searchValue, setSearchValue] = useState("");
   const [bookList, setBookList] = useState([]);
-  const makeAxiosCall = () => {};
+  const [searchLoading, setSearchLoading] = useState(false);
   const handleSearchValueChange = (search) => {
     setSearchValue(search);
   };
+  const handleLongPressTitle = async (e) => {
+    let bookTitle = e?._targetInst?.child?.memoizedProps;
+    alert("Copied to clipboard");
+    await Clipboard.setStringAsync(bookTitle);
+  };
+  const handleEmptySearch = () => setBookList([]);
+
   const handleSubmitSearch = () => {
-    axios
-      .get(
-        `https://biblocate.azurewebsites.net/api/Books/SearchBooks/${searchValue}/100`
-      )
-      .then(function (response) {
-        setBookList(response?.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    if (searchValue == "") handleEmptySearch();
+    else {
+      setSearchLoading(true);
+      axios
+        .get(
+          `https://biblocate.azurewebsites.net/api/Books/SearchBooks/${searchValue}/100`
+        )
+        .then(function (response) {
+          setBookList(response?.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .finally(() => {
+          setSearchLoading(false);
+        });
+    }
   };
   return (
     <SafeAreaView style={styles.container}>
       <SearchBar
+        showLoading={searchLoading}
         placeholder="Title"
         lightTheme="true"
         value={searchValue}
         onChangeText={handleSearchValueChange}
+        onClear={handleEmptySearch}
         platform="ios"
         onSubmitEditing={handleSubmitSearch}
       />
@@ -48,8 +58,15 @@ const NewBookSearchScreen = () => {
               containerStyle={styles.listItem}
               bottomDivider
             >
+              <Ionicons
+                name="location"
+                color="white"
+                style={{ size: "large" }}
+              />
               <ListItem.Content>
-                <ListItem.Title>{book.Title}</ListItem.Title>
+                <ListItem.Title onLongPress={handleLongPressTitle}>
+                  {book.Title}
+                </ListItem.Title>
                 <ListItem.Subtitle>{book.Author}</ListItem.Subtitle>
               </ListItem.Content>
             </ListItem>
@@ -68,7 +85,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#5A6EE1",
   },
   listItem: {
-    backgroundColor: "yellow",
+    backgroundColor: "#9999ff",
+    marginBottom: 8,
   },
 });
 
