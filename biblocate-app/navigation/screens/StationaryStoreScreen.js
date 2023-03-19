577a4638db3bba722b000686711f79ca77e2c2ef
Emre Tarakci <tarakci.software@gmail.com>
2023-03-19 16:59:47 +0300

    expo segment created

--- biblocate-app/navigation/screens/StationaryStoreScreen.js
+++ biblocate-app/navigation/screens/StationaryStoreScreen.js
@@ -1,12 +1,55 @@
-import { View, Text } from "react-native";
+import { View as GraphicsView } from "expo-graphics";
+import ExpoTHREE, { THREE } from "expo-three";
 import React from "react";
 
-const StationaryStoreScreen = () => {
-  return (
-    <View>
-      <Text>StationaryStoreScreen</Text>
-    </View>
-  );
-};
+export default class App extends React.Component {
+  componentWillMount() {
+    THREE.suppressExpoWarnings();
+  }
 
-export default StationaryStoreScreen;
+  render() {
+    // Create an `ExpoGraphics.View` covering the whole screen, tell it to call our
+    // `onContextCreate` function once it's initialized.
+    return (
+      <GraphicsView
+        onContextCreate={this.onContextCreate}
+        onRender={this.onRender}
+      />
+    );
+  }
+
+  // This is called by the `ExpoGraphics.View` once it's initialized
+  onContextCreate = async ({
+    gl,
+    canvas,
+    width,
+    height,
+    scale: pixelRatio,
+  }) => {
+    this.renderer = new ExpoTHREE.Renderer({ gl, pixelRatio, width, height });
+    this.renderer.setClearColor(0xffffff);
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    this.camera.position.z = 5;
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xff0000,
+    });
+
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    this.scene.add(new THREE.AmbientLight(0x404040));
+
+    const light = new THREE.DirectionalLight(0xffffff, 0.5);
+    light.position.set(3, 3, 3);
+    this.scene.add(light);
+  };
+
+  onRender = (delta) => {
+    this.cube.rotation.x += 3.5 * delta;
+    this.cube.rotation.y += 2 * delta;
+    this.renderer.render(this.scene, this.camera);
+  };
+}
