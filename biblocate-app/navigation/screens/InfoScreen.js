0b027c1a1e0df2ec7ebcf94cdfcbb6a8fc1387db
Emre Tarakci <tarakci.software@gmail.com>
2023-03-27 22:53:13 +0300

    Added Wayfinding file

--- biblocate-app/navigation/screens/InfoScreen.js
+++ biblocate-app/navigation/screens/InfoScreen.js
@@ -1,11 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 import BookSearchScreen from "./BookSearchScreen";
+import Wayfinding from "./Wayfinding";
 
 const InfoScreen = () => {
   return (
     <View>
-      <Text>InfoScreen</Text>
+      <Wayfinding />
     </View>
   );
 };
