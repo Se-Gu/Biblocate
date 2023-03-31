0fe7c655a0ead7e86ee6e6ede33194301fdfc458
Emre Tarakci <tarakci.software@gmail.com>
2023-03-31 17:28:34 +0300

    Added three.js

--- biblocate-app/navigation/screens/CoffeeBreakEntranceScreen.js
+++ biblocate-app/navigation/screens/CoffeeBreakEntranceScreen.js
@@ -1,7 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
+import * as THREE from "three";
 
 const CoffeeBreakEntranceScreen = () => {
+  // create three js scene
+  const scene = new THREE.Scene();
   return (
     <View>
       <Text>CoffeeBreakEntranceScreen</Text>
